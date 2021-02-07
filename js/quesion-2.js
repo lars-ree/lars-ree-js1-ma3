4aca9bd7b05495fa172881446f359dd92e327a62
lars-ree <70200269+lars-ree@users.noreply.github.com>
2021-02-07 21:01:58 +0100

    done

--- js/quesion-2.js
+++ js/quesion-2.js
@@ -1,26 +1,26 @@
-const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+const url =
+  "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
 
 const container = document.querySelector(".games");
 
 async function games() {
-    try {
-        const response = await fetch(url);
-        const results = await response.json();
-        const facts = results.results;
+  try {
+    const response = await fetch(url);
+    const results = await response.json();
+    const facts = results.results;
 
-        container.innerHTML = "";
+    container.innerHTML = "";
 
-        for (let i =0; i < facts.length; i++) {
-            if (i === 8) {
-                break;
-            }
-
-            container.innerHTML += `<div class="game"><h4>${facts[i].name}</h4><p>Rating: ${facts[i].rating}</p><p>Tags:${facts[i].tags.length}</p></div>`;
-        }
-    } catch (error) {
-        console.log("An has error occurred");
-        container.innerHTML = displayError("Unable to call API");
+    for (let i = 0; i < facts.length; i++) {
+      if (i === 8) {
+        break;
+      }
+      container.innerHTML += `<div class="game"><h4>${facts[i].name}</h4><p>Rating: ${facts[i].rating}</p><p>Tags:${facts[i].tags.length}</p></div>`;
     }
+  } catch (error) {
+    console.log("An has error occurred");
+    container.innerHTML = displayError("Unable to call API");
+  }
 }
 
 games();
